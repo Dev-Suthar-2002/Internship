44c0161c6e788f42f2e45e8c197aeeafde3d982e
Dev-Suthar-2002 <123891197+Dev-Suthar-2002@users.noreply.github.com>
2024-02-08 16:14:41 +0530

    Delete elecon-API-master directory

--- elecon-API-master/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {createEmployee, updateEmployee, deleteEmployee} = require("../controllers/admin");
-router.post('/createEmployee', createEmployee)
-router.post('/updateEmployee/:id', updateEmployee)
-router.delete('/deleteEmployee/:id', deleteEmployee)
-
-
-module.exports = router
